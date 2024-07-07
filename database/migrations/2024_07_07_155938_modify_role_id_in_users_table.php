5cab0f06c5b6b8b9ed85d468a0e34d985aae37b5
thilinitharuka <138451512+thilinitharuka@users.noreply.github.com>
2024-07-07 21:37:48 +0530

    create new database table.Name => role type Add new column to the users table.(role_id)

---
+++ database/migrations/2024_07_07_155938_modify_role_id_in_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->default(2)->change(); // Modify the role_id column to have a default value of 2
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->nullable()->change(); // Revert the role_id column modification
+        });
+    }
+};
